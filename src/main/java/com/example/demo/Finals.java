3b0ebfe24253aa844b736b05201b8a42c3aa0412
Your Name <you@example.com>
2018-10-20 17:11:30 +0300

    f

--- src/main/java/com/example/demo/Finals.java
+++ src/main/java/com/example/demo/Finals.java
@@ -2,8 +2,5 @@
 
 public interface Finals {
 
-	//public static final String USERNAME = "liran9";
-    //public static final String AUTOMATE_KEY = "5xPb6EstKsyzvzd4vfFp";
-	//public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
-	public static final String facebook = "https://www.facebook.com/";
+	
 }
